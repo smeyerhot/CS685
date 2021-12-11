4520f333e3e5ee84f22f88b2a08e888824ebe7a7
Kate Avery <kteavery@users.noreply.github.com>
2021-12-11 14:20:15 -0500

    run final decoder and prefix

--- data.py
+++ data.py
@@ -81,7 +81,7 @@ def split_data(dataset, sizes):
 
 class GPT2Dataset(Dataset):
 
-  def __init__(self, df, encoder_tokenizer, decoder_tokenizer=None, gpt2_type="gpt2", max_length=768, decoder_only=True, eos=False):
+  def __init__(self, df, encoder_tokenizer, decoder_tokenizer=None, gpt2_type="gpt2", max_length=768, decoder_only=True, eos=False, padding=True):
 
     self.encoder_tokenizer = encoder_tokenizer
     self.decoder_tokenizer = decoder_tokenizer
@@ -98,26 +98,39 @@ class GPT2Dataset(Dataset):
     decoder_txt = df.decoder.copy()
 
     for i in range(len(encoder_txt)):
-      if decoder_only:
-        encoder_dict = encoder_tokenizer(encoder_txt[i][0] + encoder_tokenizer.eos_token, truncation=True, max_length=max_length, padding="max_length")
-        decoder_dict = encoder_tokenizer(decoder_txt[i][0] + encoder_tokenizer.eos_token, truncation=True, max_length=max_length, padding="max_length")
-        both_dict = encoder_tokenizer(encoder_txt[i][0] + decoder_txt[i][0] + encoder_tokenizer.eos_token, truncation=True, max_length=max_length, padding="max_length")
-      else:
-        if eos:
-            encoder_dict = encoder_tokenizer(encoder_txt[i][0] + encoder_tokenizer.eos_token, truncation=True, max_length=max_length, padding="max_length")
-            decoder_dict = decoder_tokenizer(decoder_txt[i][0] + decoder_tokenizer.eos_token, truncation=True, max_length=max_length, padding="max_length")
+        if padding:
+            if decoder_only:
+                encoder_dict = encoder_tokenizer(encoder_txt[i][0] + encoder_tokenizer.eos_token, truncation=True, max_length=max_length, padding="max_length")
+                decoder_dict = encoder_tokenizer(decoder_txt[i][0] + encoder_tokenizer.eos_token, truncation=True, max_length=max_length, padding="max_length")
+                both_dict = encoder_tokenizer(encoder_txt[i][0] + decoder_txt[i][0] + encoder_tokenizer.eos_token, truncation=True, max_length=max_length, padding="max_length")
+            else:
+                if eos:
+                    encoder_dict = encoder_tokenizer(encoder_txt[i][0] + encoder_tokenizer.eos_token, truncation=True, max_length=max_length, padding="max_length")
+                    decoder_dict = decoder_tokenizer(decoder_txt[i][0] + decoder_tokenizer.eos_token, truncation=True, max_length=max_length, padding="max_length")
+                else:
+                    encoder_dict = encoder_tokenizer(encoder_txt[i][0], truncation=True, max_length=max_length, padding="max_length")
+                    decoder_dict = decoder_tokenizer(decoder_txt[i][0], truncation=True, max_length=max_length, padding="max_length")
         else:
-            encoder_dict = encoder_tokenizer(encoder_txt[i][0], truncation=True, max_length=max_length, padding="max_length")
-            decoder_dict = decoder_tokenizer(decoder_txt[i][0], truncation=True, max_length=max_length, padding="max_length")
-      
-      self.decoder_ids.append(torch.tensor(decoder_dict['input_ids']))
-      self.decoder_attn_masks.append(torch.tensor(decoder_dict['attention_mask']))
+            if decoder_only:
+                encoder_dict = encoder_tokenizer(encoder_txt[i][0] + encoder_tokenizer.eos_token, truncation=True, max_length=max_length)
+                decoder_dict = encoder_tokenizer(decoder_txt[i][0] + encoder_tokenizer.eos_token, truncation=True, max_length=max_length)
+                both_dict = encoder_tokenizer(encoder_txt[i][0] + decoder_txt[i][0] + encoder_tokenizer.eos_token, truncation=True, max_length=1000)
+            else:
+                if eos:
+                    encoder_dict = encoder_tokenizer(encoder_txt[i][0] + encoder_tokenizer.eos_token, truncation=True, max_length=max_length)
+                    decoder_dict = decoder_tokenizer(decoder_txt[i][0] + decoder_tokenizer.eos_token, truncation=True, max_length=max_length)
+                else:
+                    encoder_dict = encoder_tokenizer(encoder_txt[i][0], truncation=True, max_length=max_length)
+                    decoder_dict = decoder_tokenizer(decoder_txt[i][0], truncation=True, max_length=max_length)
+            
+        self.decoder_ids.append(torch.tensor(decoder_dict['input_ids']))
+        self.decoder_attn_masks.append(torch.tensor(decoder_dict['attention_mask']))
 
-      self.input_ids.append(torch.tensor(encoder_dict['input_ids']))
-      self.attn_masks.append(torch.tensor(encoder_dict['attention_mask']))
+        self.input_ids.append(torch.tensor(encoder_dict['input_ids']))
+        self.attn_masks.append(torch.tensor(encoder_dict['attention_mask']))
 
-      self.both_ids.append(torch.tensor(both_dict["input_ids"]))
-      self.both_attn_masks.append(torch.tensor(both_dict["attention_mask"]))
+        self.both_ids.append(torch.tensor(both_dict["input_ids"]))
+        self.both_attn_masks.append(torch.tensor(both_dict["attention_mask"]))
     
   def __len__(self):
     return len(self.input_ids)
