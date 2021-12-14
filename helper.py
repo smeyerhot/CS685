0c446f96124ac6f70e25b0aab995ca4344d0b9d0
Kate Avery <kteavery@users.noreply.github.com>
2021-12-14 18:15:51 -0500

    evaluation functions and plots

--- helper.py
+++ helper.py
@@ -1,5 +1,8 @@
 import datetime
-from rouge_score import rouge_scorer
+# from rouge_score import rouge_scorer
+import pandas as pd
+import numpy as np
+from sklearn.metrics import cohen_kappa_score
 
 def format_time(elapsed):
     return str(datetime.timedelta(seconds=int(round((elapsed)))))
@@ -8,7 +11,97 @@ def rouge(predicted, ground):
     scorer = rouge_scorer.RougeScorer(['rouge1', 'rouge2', 'rougeL', 'rougeLsum'], use_stemmer=True)
     return scorer.score(predicted, ground)
 
+def ci95(column):
+    stddev = np.std(column) 
+    sqrtn = np.sqrt(len(column))
+    return 1.960*(stddev/sqrtn)
+
+def avgs_ci(csv):
+    df = pd.read_csv(csv)
+    print("mean")
+    print(df.mean(axis=0))
+    print("95 percent confidence interval")
+    print(df.apply(ci95,axis=0))
+
+def avg_evals(csvs):
+    question_list = []
+    fluency_list = []
+
+    for i, csvlist in enumerate(csvs):
+        df1 = pd.read_csv(csvlist[0]) 
+        df2 = pd.read_csv(csvlist[1]) 
+
+        avg_question = (df1["question_score"] + df2["question_score"])/2
+        avg_fluency = (df1["fluency_score"] + df2["fluency_score"])/2
+        
+        question_list.append(avg_question)
+        fluency_list.append(avg_fluency)
+
+    questions = pd.concat(question_list)
+    fluency = pd.concat(fluency_list)
+
+    print(questions.mean())
+    print(fluency.mean())
+
+
+def agreements(csvs):
+    annotator1_list = []
+    annotator2_list = []
+
+    for i, csvlist in enumerate(csvs):
+        df1 = pd.read_csv(csvlist[0]) 
+        df2 = pd.read_csv(csvlist[1]) 
+
+        annotator1_list.append(df1)
+        annotator2_list.append(df2)
+
+    annotator1 = pd.concat(annotator1_list)
+    annotator2 = pd.concat(annotator2_list)
+
+    print("question score")
+    same = annotator1["question_score"].values==annotator2["question_score"].values
+    raw_agreement = list(same).count(True) / len(annotator1["question_score"])
+    # print(raw_agreement)
+
+    cohens = cohen_kappa_score(annotator1["question_score"], annotator2["question_score"], weights="linear")
+    print(cohens)
+
+    print("fluency")
+    same = annotator1["fluency_score"].values==annotator2["fluency_score"].values
+    raw_agreement = list(same).count(True) / len(annotator1["fluency_score"])
+    # print(raw_agreement)
+
+    cohens = cohen_kappa_score(annotator1["fluency_score"], annotator2["fluency_score"], weights="linear")
+    print(cohens)
+
+
 if __name__=='__main__':
-    score = rouge("I have a dry cough. Could I have COVID-19?In brief:   Fever, dry cough, headache, body aches.   If you have access to testing you can do that.   Would you like to video or text chat with me?", 
-        "Perhaps. If you have access to testing, you should do that.")
-    print(score)
+    # score = rouge("I have a dry cough. Could I have COVID-19?In brief:   Fever, dry cough, headache, body aches.   If you have access to testing you can do that.   Would you like to video or text chat with me?", 
+    #     "Perhaps. If you have access to testing, you should do that.")
+    # print(score)
+
+    # avgs_ci("/Users/kavery/Documents/rouge_prefix.csv")
+
+    avg_evals([["/Users/kavery/Downloads/processed_baselines/baseline1_kate.csv", 
+                "/Users/kavery/Downloads/processed_baselines/baseline1_mina.csv"], \
+              ["/Users/kavery/Downloads/processed_baselines/baseline2_kate.csv", 
+              "/Users/kavery/Downloads/processed_baselines/baseline2_mina.csv"], \
+              ["/Users/kavery/Downloads/processed_baselines/baseline3_kate.csv",
+              "/Users/kavery/Downloads/processed_baselines/baseline3_mina.csv"], \
+              ["/Users/kavery/Downloads/processed_baselines/baseline4_kate.csv",
+              "/Users/kavery/Downloads/processed_baselines/baseline4_mina.csv"], \
+              ["/Users/kavery/Downloads/processed_baselines/baseline5_kate.csv",
+              "/Users/kavery/Downloads/processed_baselines/baseline5_teo.csv"]]
+              )
+
+    agreements([["/Users/kavery/Downloads/processed_baselines/baseline1_kate.csv", 
+                "/Users/kavery/Downloads/processed_baselines/baseline1_mina.csv"], \
+              ["/Users/kavery/Downloads/processed_baselines/baseline2_kate.csv", 
+              "/Users/kavery/Downloads/processed_baselines/baseline2_mina.csv"], \
+              ["/Users/kavery/Downloads/processed_baselines/baseline3_kate.csv",
+              "/Users/kavery/Downloads/processed_baselines/baseline3_mina.csv"], \
+              ["/Users/kavery/Downloads/processed_baselines/baseline4_kate.csv",
+              "/Users/kavery/Downloads/processed_baselines/baseline4_mina.csv"], \
+              ["/Users/kavery/Downloads/processed_baselines/baseline5_kate.csv",
+              "/Users/kavery/Downloads/processed_baselines/baseline5_teo.csv"]]
+              )
--- plots.py
+++ plots.py
@@ -1,5 +1,6 @@
 import seaborn as sns
 import matplotlib.pyplot as plt
+import pandas as pd
 
 def loss_curves(df_stats):
     # Use plot styling from seaborn.
@@ -18,6 +19,41 @@ def loss_curves(df_stats):
     plt.xlabel("Epoch")
     plt.ylabel("Loss")
     plt.legend()
-    plt.xticks([1, 2, 3, 4])
+    plt.xticks([0, 1, 2, 3])
+    plt.yticks([0, 0.5, 1.0, 1.5])
 
     plt.show()
+
+def plot_loss_csvs(csvs):
+    training = pd.DataFrame()
+    validation = pd.DataFrame()
+    for i, csv in enumerate(csvs):
+        df = pd.read_csv(csv)
+        print(df.head())
+        print(df.columns)
+        training[str(i)] = df["training"]
+        validation[str(i)] = df[" validation"]
+    
+    training['mean'] = training.mean(axis=1)
+    validation['mean'] = validation.mean(axis=1)
+    stats = pd.DataFrame()
+    stats["Training Loss"] = training["mean"]
+    stats["Valid. Loss"] = validation["mean"]
+    
+    print(training)
+    print(validation)
+
+    loss_curves(stats)
+
+if __name__=='__main__':
+    plot_loss_csvs(["/Users/kavery/Downloads/loss/decoder1_loss.csv", \
+                    "/Users/kavery/Downloads/loss/decoder2_loss.csv", \
+                    "/Users/kavery/Downloads/loss/decoder3_loss.csv", \
+                    "/Users/kavery/Downloads/loss/decoder4_loss.csv", \
+                    "/Users/kavery/Downloads/loss/decoder5_loss.csv" ])
+
+    plot_loss_csvs(["/Users/kavery/Downloads/loss/prefix1_loss.csv", \
+                    "/Users/kavery/Downloads/loss/prefix2_loss.csv", \
+                    "/Users/kavery/Downloads/loss/prefix3_loss.csv", \
+                    "/Users/kavery/Downloads/loss/prefix4_loss.csv", \
+                    "/Users/kavery/Downloads/loss/prefix5_loss.csv" ])
